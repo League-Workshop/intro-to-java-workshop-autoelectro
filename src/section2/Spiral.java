7c2ba5c93c4d776dc3beb23516a7a9bb172a0bf2
DavidThorpe123 <league@worthymini-5.attlocal.net>
2018-10-07 11:53:56 -0700

    I made a design using pentagons and other shapes.

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,22 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot ree=new Robot();
 		// 5. Set your robot's pen down 
-		
+		ree.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+	ree.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 0; i < 550; i++) {
+			
 		
 			// 7. Change the pen color to random
-		
-			// 6. Move the robot 5 times the loop counter (5*i)
-			
+		ree.setRandomPenColor();
+			// 6. Move the robot 5 times the loop counter (i)
+			ree.move(i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		ree.turn(360/7);
 			// 8. Set the pen width to i
-			
+		ree.setPenWidth(i);}
 	}
 }
