0e0dfcb5e79b12dbd5675c2d64bfbc9d47c83e4a
pigchick3n <league@WorthyMini-5.attlocal.net>
2018-10-13 11:51:24 -0700

    day 3 of intro
    New programs

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -5,14 +5,15 @@ import javax.swing.JOptionPane;
 public class CandyCrush {
 
 	public static void main(String[] args) {
+		String AliA;
 		// 1. Using a pop-up, ask the user who they don't like
-
+AliA=JOptionPane.showInputDialog("What do you not like?");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You totally have a crush on" + AliA);
 		// 3. Ask the user for the name of their best friend
-
+AliA=JOptionPane.showInputDialog("Who's your best friend?");
 		// 4. Tell them their best friend is as sweet as candy
-
+JOptionPane.showMessageDialog(null, "you're as sweet as candy!");
 	} 
 }
 
---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,15 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+public static void main(String[] args) {
+String Shrek;	
+JOptionPane.	showMessageDialog(null, "You look cool I don't dislike you anymore and you are not as mean");
+Shrek=JOptionPane.showInputDialog("Why are you bullying me?");
+JOptionPane.showMessageDialog(null, Shrek + "Ur shoes are too green. BAM BAM eeeeeeeeeeee");
+	
+
+}
+
+}
